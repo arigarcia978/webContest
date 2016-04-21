6bcf292b0250802a3dd521d6fd6ff4b8b4f07948
Ariana Garcia <arigarcia.978@gmail.com>
2016-04-21 12:04:41 -0300

    Main page added

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('app', ['ui.router']).
+	config(['$stateProvider', '$urlRouterProvider', 
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('main', {
+					url: '/',
+					templateUrl: 'views/main.html'
+				})
+				.state('about', {
+					url: '/about',
+					templateUrl: 'views/aboutus.html'
+				})
+				.state('contact', {
+					url: '/contact',
+					templateUrl: 'views/contactus.html'
+				})
+				.state('ieee_info', {
+					url: '/ieee',
+					templateUrl: 'views/ieee.html'
+				});
+
+			$urlRouterProvider.otherwise('/');
+		}
+	]);
